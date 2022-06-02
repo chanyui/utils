2cbd63b53472c470961ce66868aa9fc50730a2ce
yc <718653142@qq.com>
2022-06-02 11:17:10 +0800

    new utils

--- examples/mail/mailer.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-require_once __DIR__ . '/../../autoload.php';
-
-use chanyu\Mail\Mailer;
-
-$mail = new Mailer(['host' => '', 'username' => '', 'password' => '', 'from' => '', 'name' => '系统邮件']);
-var_dump($mail->send(['address' => ''], ['subject' => '测试', 'type' => 'html', 'body' => '<b>测试</b>']));
--- src/Mail/Contract.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace chanyu\Mail;
-
-interface Contract
-{
-    public function send($tomail, $payload, $reply = [], $cc = [], $attachment = []);
-}
